fee715187010c5f76e2e1e397ae377d3c5aa1d6d
Treeesk <yaroslav.kalinin.06@yandex.ru>
2026-03-19 17:36:47 +0300

    The AreaCalculator class, which violates OCP, has been removed, along with other inappropriate classes.

--- ConsoleApp2/ocp/Case2.cs
+++ ConsoleApp2/ocp/Case2.cs
@@ -8,103 +8,48 @@ namespace ConsoleApp2.ocp
 {
     class Case2
     {
-        class Circle
-        {
-            public double Radius;
-            public Circle(double radius)
-            {
-                Radius = radius;
-            }
-            public double GetArea()
-            {
-                return Math.PI * Radius * Radius;
-            }
-        }
-        class Rectangle
-        {
-            public double Width;
-            public double Height;
-            public Rectangle(double width, double height)
-            {
-                Width = width;
-                Height = height;
-            }
-            public double GetArea()
-            {
-                return Width * Height;
-            }
-        }
-        class AreaCalculator
-        {
-            public double Calculate(object shape)
-            {
-                if (shape is Circle)
-                {
-                    return ((Circle)shape).GetArea();
-                }
-                else if (shape is Rectangle)
-                {
-                    return ((Rectangle)shape).GetArea();
-                }
-                return 0;
-            }
-        }
         class Program
         {
             static void Main()
             {
                 Circle c = new Circle(5);
                 Rectangle r = new Rectangle(4, 6);
-                AreaCalculator calculator = new AreaCalculator();
-                Console.WriteLine("Circle area: " + calculator.Calculate(c));
-                Console.WriteLine("Rectangle area: " + calculator.Calculate(r));
+                Console.WriteLine("Circle area: " + c.GetArea());
+                Console.WriteLine("Rectangle area: " + r.GetArea());
             }
         }
 
 
-        //class Figure
-        //{
-        //    public virtual double GetArea()
-        //    {
-        //        return 0;
-        //    }
-        //}
+        abstract class Figure
+        {
+           public abstract double GetArea();
+        }
 
-        //class Circle : Figure
-        //{
-        //    public double Radius { get; set; }
-        //    public Circle(double radius)
-        //    {
-        //        Radius = radius;
-        //    }
-        //    public override double GetArea()
-        //    {
-        //        return Math.PI * Radius * Radius;
-        //    }
-        //}
-        //class Rectangle : Figure
-        //{
-        //    public double Width { get; set; }
-        //    public double Height { get; set; }
-        //    public Rectangle(double width, double height)
-        //    {
-        //        Width = width;
-        //        Height = height;
-        //    }
-        //    public override double GetArea()
-        //    {
-        //        return Width * Height;
-        //    }
-        //}
-        //class Application
-        //{
-        //    static void Main()
-        //    {
-        //        var c = new Circle(5);
-        //        var r = new Rectangle(4, 6);
-        //        Console.WriteLine("Circle area: " + c.GetArea());
-        //        Console.WriteLine("Rectangle area: " + r.GetArea());
-        //    }
-        //}
+        class Circle : Figure
+        {
+           public double Radius { get; set; }
+           public Circle(double radius)
+           {
+               Radius = radius;
+           }
+           public override double GetArea()
+           {
+               return Math.PI * Radius * Radius;
+           }
+        }
+        class Rectangle : Figure
+        {
+           public double Width { get; set; }
+           public double Height { get; set; }
+           public Rectangle(double width, double height)
+           {
+               Width = width;
+               Height = height;
+           }
+           public override double GetArea()
+           {
+               return Width * Height;
+           }
+        }
     }
 }
